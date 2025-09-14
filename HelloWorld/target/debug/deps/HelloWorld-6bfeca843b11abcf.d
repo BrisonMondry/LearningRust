4a4e72fb292bda544a17ae4827de1b96c4ad4452
Brison Mondry <mondrybr@gmail.com>
2025-09-14 17:50:55 -0400

    Starting up!

---
+++ HelloWorld/target/debug/deps/HelloWorld-6bfeca843b11abcf.d
@@ -0,0 +1,5 @@
+C:\Users\Owner\Documents\LearningRust\HelloWorld\target\debug\deps\HelloWorld-6bfeca843b11abcf.d: src\main.rs
+
+C:\Users\Owner\Documents\LearningRust\HelloWorld\target\debug\deps\libHelloWorld-6bfeca843b11abcf.rmeta: src\main.rs
+
+src\main.rs:
---
+++ HelloWorld/target/debug/deps/HelloWorld-9cdbf1880f71af74.d
@@ -0,0 +1,5 @@
+C:\Users\Owner\Documents\LearningRust\HelloWorld\target\debug\deps\HelloWorld-9cdbf1880f71af74.d: src\main.rs
+
+C:\Users\Owner\Documents\LearningRust\HelloWorld\target\debug\deps\libHelloWorld-9cdbf1880f71af74.rmeta: src\main.rs
+
+src\main.rs:
